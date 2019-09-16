e4f5f6f549852d8a9fb4fe7510d8d6d9410581b4
Luke Brannigan <lukebrannigan57@gmail.com>
2019-09-16 15:56:44 -0400

    updating changes to employee, receptionist

--- src/main/java/Receptionist.java
+++ src/main/java/Receptionist.java
@@ -1,4 +1,12 @@
 
-public class Receptionist {
+public class Receptionist extends Employee implements OnThePhone{
+
+	public Receptionist(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
 
 }
--- src/test/java/ReceptionistTest.java
+++ src/test/java/ReceptionistTest.java
@@ -1,4 +1,4 @@
 
-public class ReceptionistTest {
+public class ReceptionistTest  {
 
 }
