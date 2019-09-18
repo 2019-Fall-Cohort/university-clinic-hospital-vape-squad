8c4ffc8a7da053c9dfd56044ce0b2ccb320c7c05
Luke Brannigan <lukebrannigan57@gmail.com>
2019-09-18 16:49:18 -0400

    added on the phone to receptionist

--- src/main/java/hospital/Receptionist.java
+++ src/main/java/hospital/Receptionist.java
@@ -7,10 +7,18 @@ public class Receptionist extends Employee implements OnThePhone{
 		// TODO Auto-generated constructor stub
 	}
 
-	
 	public boolean receptionistOnThePhone(boolean onThePhone) {
+		if (onThePhone) {
+			return true;
+		}
 		// TODO Auto-generated method stub
 		return false; 
+		
+	}
+
+	@Override
+	public void onThePhone() {
+		// TODO Auto-generated method stub
 		
 	}
 	
